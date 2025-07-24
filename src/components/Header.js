32160cfd3db920c81e124c476d300216746c712e
adeel-58 <khuffia58@gmail.com>
2025-07-24 16:16:53 +0500

    made until talktous

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,9 @@ import phoneIcon from '../icons/phone.svg';
 import emailIcon from '../icons/email.svg';
 import hamburgerIcon from '../icons/hamburger.svg';
 
+;
+
+
 const Header = () => {
   // State to track if the page has been scrolled
   const [scrolled, setScrolled] = useState(false);
@@ -30,29 +33,32 @@ const Header = () => {
 
   return (
     // Conditionally apply the 'scrolled' class based on the 'scrolled' state
-    <header className={`header-container ${scrolled ? 'scrolled' : ''}`}>
-      <div className="header-top">
-        <div className="contact-info">
-          <img src={phoneIcon} alt="Phone" />
-          <span>+40 758 577 624</span>
-          <img src={emailIcon} alt="Email" />
-          <span>bizfuturix@gmail.com</span>
-        </div>
-        <div className="social-links">
-          <button>Facebook</button>
-          <button>Instagram</button>
-          <button>LinkedIn</button>
-        </div>
+   <header className={`header-container ${scrolled ? 'scrolled' : ''}`}>
+  {!scrolled && (
+    <div className="header-top">
+      <div className="contact-info">
+        <img src={phoneIcon} alt="Phone" />
+        <span>+40 758 577 624</span>
+        <img src={emailIcon} alt="Email" />
+        <span>bizfuturix@gmail.com</span>
       </div>
+      <div className="social-links">
+        <button>Facebook</button>
+        <button>Instagram</button>
+        <button>LinkedIn</button>
+      </div>
+    </div>
+  )}
 
-      <div className="header-bottom">
-        <img src={logo} alt="Bizfuturix Logo" className="logo" />
-        <div className="right-controls">
-          <button className="project-btn">START A PROJECT</button>
-          <img src={hamburgerIcon} alt="Menu" className="hamburger" />
-        </div>
-      </div>
-    </header>
+  <div className="header-bottom">
+   <img src={logo} alt="Bizfuturix Logo" className="logo" />
+    <div className="right-controls">
+      <button className="project-btn">START A PROJECT</button>
+      <img src={hamburgerIcon} alt="Menu" className="hamburger" />
+    </div>
+  </div>
+</header>
+
   );
 };
 
